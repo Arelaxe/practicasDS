38b2d5548ae6328b6e781820f78c07e4590b6f41
arelaxe <escaleranm@gmail.com>
2020-02-19 15:18:49 +0100

    Ruby

---
+++ DS_P1S1_Ruby/lib/main.rb
@@ -0,0 +1,7 @@
+#encoding: utf-8
+
+module DS_P1S1_Ruby
+  class Main
+    
+  end
+end
---
+++ DS_P1S1_Ruby/lib/prototipo_bicicleta_y_carrera.rb
@@ -0,0 +1,9 @@
+#encoding: utf-8
+
+module DS_P1S1_Ruby
+  class PrototipoBicicletaYCarrera
+    def initialize
+    
+    end
+  end
+end 
