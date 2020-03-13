fba5975a38d1a05fe1cd87b26316518d07810521
arelaxe <escaleranm@gmail.com>
2020-03-13 20:30:18 +0100

    En ventanas

--- p1s3_v2/p1s3/src/p1s3/GraficaTemperatura.java
+++ p1s3_v2/p1s3/src/p1s3/GraficaTemperatura.java
@@ -1,18 +1,58 @@
 package p1s3;
-
 import java.util.*;
 
-public class GraficaTemperatura implements Observer{
+public class GraficaTemperatura extends javax.swing.JFrame implements Observer{
     private Temperatura temperatura;
-    private VentanaPrincipal v;
-    
-    public void setVentana (VentanaPrincipal v){
-        this.v = v;
+
+    public GraficaTemperatura() {
+        setLocation(0,0);
+        initComponents();
     }
     
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jProgressBar1 = new javax.swing.JProgressBar();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("GŔAFICA TEMPERATURA");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(124, 124, 124)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1)
+                    .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(128, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addComponent(jLabel1)
+                .addGap(88, 88, 88)
+                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(151, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+  
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JProgressBar jProgressBar1;
+    // End of variables declaration//GEN-END:variables
     @Override
     public void update(Observable obs, Object obj){
         temperatura = (Temperatura) obj;
-        v.getGrafica().setValue((int)(temperatura.getTemperatura()*100)/50);
+        jProgressBar1.setValue((int)(temperatura.getTemperatura()*100)/50);
     }
 }
